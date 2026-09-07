c842ea86bf1669c6a4add50743a010a21ea649f4
Lucas Povolo <lucas.povolo1@gmail.com>
2026-09-06 21:18:49 -0300

    inicializacion de modulos, inicio del desarrollo del header:
    navbar con secciones.
    banner con imagen.

---
+++ scripts/modulos.js
@@ -0,0 +1,17 @@
+async function cargaModular()
+{
+    const header = document.getElementById("header");
+    const footer = document.getElementById("footer");
+
+    try {
+        const respHeader = await fetch("/pages/header.html");
+        header.innerHTML = await respHeader.text();
+
+        const respFooter = await fetch("/pages/footer.html");
+        footer.innerHTML = await respFooter.text();
+    } catch (error) {
+        console.error("Error cargando los módulos:", error);
+    }
+}
+
+cargaModular();
