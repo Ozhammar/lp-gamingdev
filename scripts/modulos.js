e4ae522fd65a83fded8af41577a712475c75a923
Lucas <lucas.povolo1@gmail.com>
2026-09-09 00:23:18 -0300

    Pelea intensa con el BOM, se modifico la carga de modulos realizando las siguientes acciones:
    -Se dividio, la funcion asincronica de cargaModular en dos:
    --cargaLayout, para cargar por unica vez el footer y el header. optimizando recursos.
    --cargaModular, ahora tiene una unica responsabilidad que es la carga del modulo respectivo.
    
    la carga del modulo se realiza por medio del trigger de un evento asociado al documento, el cual busca el evento click el cual posea la clase .btnMenu asociada, a partir de ello, se lee el dataset del boton pertinente y se llama a la funcion cargaModular, enviando el modulo como parametro y asi se rutea al html correcto

--- scripts/modulos.js
+++ scripts/modulos.js
@@ -1,25 +1,35 @@
-async function cargaModular(elemento = "home.html")
-{
-    const header = document.getElementById("header");
-    const footer = document.getElementById("footer");
-    const main  = document.getElementById("main");
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".btnMenu")) {
+    const modulo = e.target.dataset.modulo;
+    cargaModular(modulo);
+  }
+});
 
-    try {
-        const respHeader = await fetch("/pages/header.html");
-        header.innerHTML = await respHeader.text();
+async function cargaLayout() {
+  const header = document.getElementById("header");
+  const footer = document.getElementById("footer");
+  try {
+    const respHeader = await fetch("/pages/header.html");
+    header.innerHTML = await respHeader.text();
 
-        const respFooter = await fetch("/pages/footer.html");
-        footer.innerHTML = await respFooter.text();
-
-        let respMain = await fetch("/pages/modulos/"+elemento)
-        main.innerHTML = await respMain.text();
-
-
-    } catch (error) {
-        console.error("Error cargando los módulos:", error);
-    }
+    const respFooter = await fetch("/pages/footer.html");
+    footer.innerHTML = await respFooter.text();
+  } catch (error) {
+    console.error("Error cargando los módulos:", error);
+  }
 }
 
+async function cargaModular(elemento = "home.html") {
+  const main = document.getElementById("main");
 
-
+  try {
+    let respMain = await fetch("/pages/modulos/" + elemento);
+    main.innerHTML = await respMain.text();
+  } catch (error) {
+    console.error("Error cargando los módulos:", error);
+  }
+}
+cargaLayout();
 cargaModular();
+
+
